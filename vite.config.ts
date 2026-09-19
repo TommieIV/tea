b28e8b1c804149d6fc00207d411c2fc52f4ee9f6
TommieIV <tommie@tgtech.org>
2026-09-19 02:38:41 +0000

    Preserve PWA service worker update path

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     VitePWA({
       strategies: 'injectManifest',
       srcDir: 'src',
-      filename: 'service-worker.ts',
+      filename: 'sw.ts',
       registerType: 'prompt',
       includeAssets: ['tea_fav.png'],
       manifest: {
